3deb1dba9dbbdc425d005ce6a63646f67c283db0
ay7991 <amy.yang7991@gmail.com>
2023-12-07 00:33:35 -0500

    M V P

--- mini-git-code/main.js
+++ mini-git-code/main.js
@@ -1,1 +1,32 @@
+let unit = document.getElementsByTagName("title")[0].innerText;
+const userName = document.getElementsByName('octolytics-actor-login')[0].content;
+
+unit = unit.split('/');
+console.log(unit[1]);
+
+const container = document.createElement('div');
+container.setAttribute('id', 'minigitwindow');
+document.body.appendChild(container);
+
+const minigitwindow = document.getElementById('minigitwindow');
+
+minigitwindow.style.position = 'sticky';
+minigitwindow.style.bottom = '0px';
+minigitwindow.style.left = '100%';
+minigitwindow.style.width = '350px';
+minigitwindow.style.height = '400px';
+minigitwindow.style.backgroundColor = 'lightpink';
+
+// first step
+const firstStep = document.createElement('div');
+firstStep.setAttribute('id', 'firstStep');
+minigitwindow.appendChild(firstStep);
+
+const theFirstStep = document.getElementById('firstStep');
+theFirstStep.style.backgroundColor = 'lightblue';
+theFirstStep.style.width = '100%';
+theFirstStep.style.height = '50px';
+theFirstStep.innerHTML = `git clone https://github.com/${userName}/${unit[1]}.git`
+
+
 
