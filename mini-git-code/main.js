0de639d70b347cdb31932140a63c5b98133ae2d4
ay7991 <amy.yang7991@gmail.com>
2023-12-07 14:30:36 -0500

    css changes

--- mini-git-code/main.js
+++ mini-git-code/main.js
@@ -16,6 +16,7 @@ minigitwindow.style.left = '100%';
 minigitwindow.style.width = '350px';
 minigitwindow.style.height = '400px';
 minigitwindow.style.backgroundColor = 'lightpink';
+minigitwindow.style.borderRadius = '8px';
 
 // first step
 const firstStep = document.createElement('div');
@@ -26,7 +27,26 @@ const theFirstStep = document.getElementById('firstStep');
 theFirstStep.style.backgroundColor = 'lightblue';
 theFirstStep.style.width = '100%';
 theFirstStep.style.height = '50px';
-theFirstStep.innerHTML = `git clone https://github.com/${userName}/${unit[1]}.git`
+theFirstStep.style.borderRadius = '8px';
+theFirstStep.style.marginBottom = '5px';
+theFirstStep.innerText = 'Fork this repo';
+
+
+// second step
+const secondStep = document.createElement('div');
+secondStep.setAttribute('id', 'secondStep');
+minigitwindow.appendChild(secondStep);
+
+const theSecondStep = document.getElementById('secondStep');
+theSecondStep.style.display = 'flex';
+theSecondStep.style.alignItems = 'center';
+theSecondStep.style.justifyContent = 'center';
+theSecondStep.style.backgroundColor = 'lightblue';
+theSecondStep.style.width = '100%';
+theSecondStep.style.height = '50px';
+theSecondStep.style.borderRadius = '8px';
+theFirstStep.style.marginBottom = '5px';
+theSecondStep.innerText = `git clone https://github.com/${userName}/${unit[1]}.git`
 
 // Fork this repo to your repo
 // Navigate to your own repo - Double check you're there! Copy the following into your command line 
